250d08e5db4f59363443a010206d88ea58f86258
Francis Balanon <fbalanon@gmail.com>
2016-11-15 20:24:54 -0800

    testing

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
+./bin/hubot
+
 bot.hear(/Hello!/,function(res) {
     return res.send("helloooooo there!");
 });
 bot.respond(/What's your favorite color?/, function(res) {
     return res.send("I'm a robot--I only see ones and zeros!");
 });
+
