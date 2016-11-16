3e3cf8f46a18869d36aa4594fefe8a8d6c22951d
Francis Balanon <fbalanon@gmail.com>
2016-11-15 20:58:36 -0800

    adding

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-./bin/hubot
-
-bot.hear(/Hello!/,function(res) {
+module.exports = function(hero) {
+hero.hear(/Hello!/,function(res) {
     return res.send("helloooooo there!");
 });
-bot.respond(/What's your favorite color?/, function(res) {
+hero.respond(/What's your favorite color?/, function(res) {
     return res.send("I'm a robot--I only see ones and zeros!");
 });
-
+};
