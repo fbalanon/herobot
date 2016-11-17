6407ed3db8ffea684710c90276cb9d0d0a05150e
Francis Balanon <fbalanon@gmail.com>
2016-11-16 21:56:07 -0800

    change

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 module.exports = function(hero) {
-hero.hear(/Hello!/,function(res) {
-    return res.send("helloooooo there!");
-});
-hero.respond(/What's your favorite color?/, function(res) {
-    return res.send("I'm a robot--I only see ones and zeros!");
-});
+    hero.hear(/Hey bot!/,function(res) {
+        return res.send("helloooooo there!");
+    });
+    hero.respond(/What's your favorite color?/, function(res) {
+        return res.send("I'm a robot--I only see ones and zeros!");
+    });
+ /*
+ bot.respond(/Hi Hubot! My name is (.*)/i, function(msg) {
+   var name;
+   name = msg.match[1];
+   if (name == "Hubot"){
+     return msg.send("You're not Hubot--I'm Hubot!");
+   } else {
+     return msg.reply("Nice to meet you, " + name + "!");
+   }
+ });
+*/
 };
